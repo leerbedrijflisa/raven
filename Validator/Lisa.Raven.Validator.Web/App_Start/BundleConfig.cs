9e892a20b954daaaa510a2e52e006aab409f39da
LaylConway <laylconway@live.com>
2014-09-19 17:11:49 +0200

    Added simple example usage of AngularJS

--- Validator/Lisa.Raven.Validator.Web/App_Start/BundleConfig.cs
+++ Validator/Lisa.Raven.Validator.Web/App_Start/BundleConfig.cs
@@ -7,11 +7,16 @@ namespace Lisa.Raven.Validator.Web
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/site").Include(
-					  "~/Scripts/site.js"));
+			// Angular.JS
+			bundles.Add(new ScriptBundle(
+				"~/bundles/angular", "https://ajax.googleapis.com/ajax/libs/angularjs/1.2.25/angular.min.js").Include(
+					"~/Scripts/angular.min.js"));
 
+			// Custom site scritps and styling
+			bundles.Add(new ScriptBundle("~/bundles/site").Include(
+				"~/Scripts/app.js"));
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/site.css"));
+				"~/Content/site.css"));
 
 #if DEBUG
 			BundleTable.EnableOptimizations = false;
---
+++ Validator/Lisa.Raven.Validator.Web/Scripts/app.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    var app = angular.module('raven', []);
+
+    app.controller('ValidationController', function() {
+        this.valErrors = validationErrors;
+    });
+
+    var validationErrors = [
+        {
+            Message: 'The validation has been errored!',
+            Line: '2',
+            Column: '5'
+        },
+        {
+            Message: 'The error has been validationed!',
+            Line: '5',
+            Column: '4'
+        }
+    ];
+})();
