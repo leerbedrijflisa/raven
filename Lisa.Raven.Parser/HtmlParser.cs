87fcb86b3744b8f03b28f3b3aa1a71ec8b1f223c
LaylConway <laylconway@live.com>
2014-09-19 15:26:16 +0200

    Passed over HtmlParser and HtmlTokenizer with ReSharper's Clean Code.

--- Lisa.Raven.Parser/HtmlParser.cs
+++ Lisa.Raven.Parser/HtmlParser.cs
@@ -4,135 +4,140 @@ using System.Linq;
 
 namespace Lisa.Raven.Parser
 {
-    public class HtmlParser
-    {
-	    public static ParsedHtml Parse(string html)
-	    {
-			if(html == null)
+	public class HtmlParser
+	{
+		private Token _currentToken;
+		private bool _endOfSource;
+		private IEnumerable<Token> _source;
+		private IEnumerator<Token> _sourceEnumerator;
+
+		public static ParsedHtml Parse(string html)
+		{
+			if (html == null)
 				throw new ArgumentNullException("html");
 
-		    var lexer = new HtmlLexer();
-		    var lexemes = lexer.Lex(html);
+			var lexer = new HtmlLexer();
+			var lexemes = lexer.Lex(html);
 
-		    var tokenizer = new HtmlTokenizer();
+			var tokenizer = new HtmlTokenizer();
 			var tokens = tokenizer.Tokenize(lexemes).ToArray();
 
-		    var parser = new HtmlParser();
-		    var tree = parser.Parse(tokens);
+			var parser = new HtmlParser();
+			var tree = parser.Parse(tokens);
 
 			var parsed = new ParsedHtml
 			{
 				Tokens = tokens,
 				Tree = tree
 			};
-		    return parsed;
-	    }
+			return parsed;
+		}
 
-	    public SyntaxNode Parse(IEnumerable<Token> tokens)
-	    {
-		    _source = tokens;
-            _sourceEnumerator = _source.GetEnumerator();
-            NextToken();
+		public SyntaxNode Parse(IEnumerable<Token> tokens)
+		{
+			_source = tokens;
+			_sourceEnumerator = _source.GetEnumerator();
+			NextToken();
 
-            var root = ParseContent();
-            root.Type = SyntaxNodeType.DocumentRoot;
+			var root = ParseContent();
+			root.Type = SyntaxNodeType.DocumentRoot;
 
-            return root;
-        }
+			return root;
+		}
 
-        private SyntaxNode ParseElement(SyntaxNode node)
-        {
-	        node.Type = SyntaxNodeType.Element;
-	        node.Value = _currentToken.Value;
+		private SyntaxNode ParseElement(SyntaxNode node)
+		{
+			node.Type = SyntaxNodeType.Element;
+			node.Value = _currentToken.Value;
 
 			var openTagNode = ParseOpenTagNode(new SyntaxNode
 			{
 				Line = _currentToken.Line,
 				Column = _currentToken.Column
 			});
-            node.Children.Add(openTagNode);
+			node.Children.Add(openTagNode);
 
-            var contentNode = ParseContent();
-            node.Children.Add(contentNode);
+			var contentNode = ParseContent();
+			node.Children.Add(contentNode);
 
-            if (_currentToken.Value == node.Value)
-            {
+			if (_currentToken.Value == node.Value)
+			{
 				var closeTagNode = ParseCloseTagNode(new SyntaxNode
 				{
 					Line = _currentToken.Line,
 					Column = _currentToken.Column
 				});
-                node.Children.Add(closeTagNode);
-            }
+				node.Children.Add(closeTagNode);
+			}
 
-            return node;
-        }
+			return node;
+		}
 
-        private SyntaxNode ParseSelfClosingElement(SyntaxNode node)
-        {
-	        node.Type = SyntaxNodeType.Element;
-	        node.Value = _currentToken.Value;
+		private SyntaxNode ParseSelfClosingElement(SyntaxNode node)
+		{
+			node.Type = SyntaxNodeType.Element;
+			node.Value = _currentToken.Value;
 
-            var openTagNode = ParseOpenTagNode(new SyntaxNode
-            {
-	            Line = _currentToken.Line,
+			var openTagNode = ParseOpenTagNode(new SyntaxNode
+			{
+				Line = _currentToken.Line,
 				Column = _currentToken.Column
-            });
+			});
 			node.Children.Add(openTagNode);
 
 			// Previously a content node was added here, this is no longer the case.
 
-            return node;
-        }
+			return node;
+		}
 
-        private SyntaxNode ParseContent()
-        {
-	        var node = new SyntaxNode
-	        {
-		        Type = SyntaxNodeType.Content,
-		        Line = _currentToken.Line,
-		        Column = _currentToken.Column
-	        };
+		private SyntaxNode ParseContent()
+		{
+			var node = new SyntaxNode
+			{
+				Type = SyntaxNodeType.Content,
+				Line = _currentToken.Line,
+				Column = _currentToken.Column
+			};
 
-	        while (!_endOfSource)
-            {
-                var child = new SyntaxNode
-                {
-	                Line = _currentToken.Line,
+			while (!_endOfSource)
+			{
+				var child = new SyntaxNode
+				{
+					Line = _currentToken.Line,
 					Column = _currentToken.Column
-                };
+				};
 
-                switch (_currentToken.Type)
-                {
-                    case TokenType.OpenTag:
+				switch (_currentToken.Type)
+				{
+					case TokenType.OpenTag:
 						child = ParseElement(child);
-                        break;
+						break;
 
-                    case TokenType.CloseTag:
-                        return node;
+					case TokenType.CloseTag:
+						return node;
 						// Does not need a NextToken() as the close tag is not yet handled here
 
-                    case TokenType.SelfClosingTag:
+					case TokenType.SelfClosingTag:
 						child = ParseSelfClosingElement(child);
-                        break;
+						break;
 
 					case TokenType.Doctype:
 						child = ParseDoctype(child);
-		                break;
+						break;
 
 					case TokenType.Text:
 						child = ParseText(child);
-		                break;
+						break;
 
-                    default:
-		                throw new NotImplementedException();
-                }
+					default:
+						throw new NotImplementedException();
+				}
 
-                node.Children.Add(child);
-            }
+				node.Children.Add(child);
+			}
 
-            return node;
-        }
+			return node;
+		}
 
 		private SyntaxNode ParseDoctype(SyntaxNode node)
 		{
@@ -148,74 +153,69 @@ namespace Lisa.Raven.Parser
 			node.Value = "";
 
 			// Merge all sequential text tokens into this node
-		    while (!_endOfSource && _currentToken.Type == TokenType.Text)
-		    {
-			    node.Value += _currentToken.Value;
+			while (!_endOfSource && _currentToken.Type == TokenType.Text)
+			{
+				node.Value += _currentToken.Value;
 
 				NextToken();
-		    }
+			}
 
-		    return node;
-	    }
+			return node;
+		}
 
 		private SyntaxNode ParseOpenTagNode(SyntaxNode node)
 		{
 			node.Type = SyntaxNodeType.OpenTag;
 			node.Value = _currentToken.Value;
 
-            foreach (var attribute in _currentToken.Attributes)
-            {
-                var attributeNode = new SyntaxNode
-                {
-                    Type = SyntaxNodeType.Attribute,
-                    Children = new List<SyntaxNode>
-                    {
-                        new SyntaxNode
-                        {
-                            Type = SyntaxNodeType.AttributeName,
-                            Value = attribute.Name
-                        },
-                        new SyntaxNode
-                        {
-                            Type = SyntaxNodeType.AttributeValue,
-                            Value = attribute.Value
-                        }
-                    }
-                };
+			foreach (var attribute in _currentToken.Attributes)
+			{
+				var attributeNode = new SyntaxNode
+				{
+					Type = SyntaxNodeType.Attribute,
+					Children = new List<SyntaxNode>
+					{
+						new SyntaxNode
+						{
+							Type = SyntaxNodeType.AttributeName,
+							Value = attribute.Name
+						},
+						new SyntaxNode
+						{
+							Type = SyntaxNodeType.AttributeValue,
+							Value = attribute.Value
+						}
+					}
+				};
 
 				// TODO: Add the attribute node
-            }
+			}
 
-            NextToken();
+			NextToken();
 
-            return node;
-        }
+			return node;
+		}
 
-        private SyntaxNode ParseCloseTagNode(SyntaxNode node)
-        {
-	        node.Type = SyntaxNodeType.CloseTag;
+		private SyntaxNode ParseCloseTagNode(SyntaxNode node)
+		{
+			node.Type = SyntaxNodeType.CloseTag;
 			node.Value = _currentToken.Value;
 
-            NextToken();
+			NextToken();
 
-            return node;
-        }
+			return node;
+		}
 
-        private void NextToken()
-        {
-            if (!_sourceEnumerator.MoveNext())
-            {
-                _endOfSource = true;
-            }
-            else
-            {
-                _currentToken = _sourceEnumerator.Current;
-            }
-        }
-
-        private IEnumerable<Token> _source;
-        private IEnumerator<Token> _sourceEnumerator;
-        private Token _currentToken;
-        private bool _endOfSource;
-    }
+		private void NextToken()
+		{
+			if (!_sourceEnumerator.MoveNext())
+			{
+				_endOfSource = true;
+			}
+			else
+			{
+				_currentToken = _sourceEnumerator.Current;
+			}
+		}
+	}
 }
