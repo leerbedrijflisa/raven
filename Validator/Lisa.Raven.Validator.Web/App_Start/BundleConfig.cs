84207386d9683e4c38e8ddb000847de812da979c
LaylConway <laylconway@live.com>
2014-09-19 13:31:53 +0200

    Added web frontend project.

---
+++ Validator/Lisa.Raven.Validator.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Optimization;
+
+namespace Lisa.Raven.Validator.Web
+{
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/site").Include(
+					  "~/Scripts/site.js"));
+
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+					  "~/Content/site.css"));
+
+#if DEBUG
+			BundleTable.EnableOptimizations = false;
+#else
+			BundleTable.EnableOptimizations = true;
+#endif
+		}
+	}
+}
