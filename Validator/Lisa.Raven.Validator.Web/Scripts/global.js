2b19a965b1e658aab812b8673e7edff3ff1f9ca1
LaylConway <laylconway@live.com>
2014-09-23 17:47:04 +0200

    Added a little bit of placeholder jQuery code to animate the validation page.

--- Validator/Lisa.Raven.Validator.Web/Scripts/global.js
+++ Validator/Lisa.Raven.Validator.Web/Scripts/global.js
@@ -1,1 +1,6 @@
 ﻿$(document).foundation();
+$('#validationInputButton').on('click', function () {
+    $('#validationInputPage').fadeOut(400, function() {
+        $('#validationErrorsPage').fadeIn(400);
+    });
+});
