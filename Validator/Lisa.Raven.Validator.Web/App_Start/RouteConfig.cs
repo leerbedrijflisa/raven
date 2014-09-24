4fd016b0b8619a51ca0cd566390315a5f5c043ad
LaylConway <laylconway@live.com>
2014-09-24 13:57:19 +0200

    Switched web frontend to attribute routing.

--- Validator/Lisa.Raven.Validator.Web/App_Start/RouteConfig.cs
+++ Validator/Lisa.Raven.Validator.Web/App_Start/RouteConfig.cs
@@ -8,12 +8,7 @@ namespace Lisa.Raven.Validator.Web
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-			routes.MapRoute(
-				name: "Default",
-				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Validate", action = "Index", id = UrlParameter.Optional }
-			);
+			routes.MapMvcAttributeRoutes();
 		}
 	}
 }
--- Validator/Lisa.Raven.Validator.Web/Controllers/ValidateController.cs
+++ Validator/Lisa.Raven.Validator.Web/Controllers/ValidateController.cs
@@ -2,8 +2,11 @@
 
 namespace Lisa.Raven.Validator.Web.Controllers
 {
+	[RoutePrefix("validate")]
 	public class ValidateController : Controller
 	{
+		[Route]
+		[Route("~/", Name = "default")]
 		public ActionResult Index()
 		{
 			return View();
