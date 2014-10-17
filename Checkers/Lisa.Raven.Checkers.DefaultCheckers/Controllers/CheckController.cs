669a91f1d1ae1cadc4eef3792862c019dc0b11ee
arthurvanstrien <arthurvanstrien@gmail.com>
2014-10-17 16:37:18 +0200

    Added check that checks if doctype is on first Line of a document. Added check if does only exist once in a html document.

--- Checkers/Lisa.Raven.Checkers.DefaultCheckers/Controllers/CheckController.cs
+++ Checkers/Lisa.Raven.Checkers.DefaultCheckers/Controllers/CheckController.cs
@@ -13,9 +13,35 @@ namespace Lisa.Raven.Checkers.DefaultCheckers.Controllers
             var errors = new List<ValidationError>();
 
             errors.AddRange(CountCheck(html.Tree, "html"));
-            errors.AddRange(CountCheck(html.Tree, "head"));
-            errors.AddRange(CountCheck(html.Tree, "body"));
+            int htmlPosition = _elementPosition;
 
+            errors.AddRange(CountCheck(html.Tree, "head"));
+            int headPosition = _elementPosition;
+
+            errors.AddRange(CountCheck(html.Tree, "body"));
+            int bodyPosition = _elementPosition;
+
+            /*
+
+            if(htmlPosition > headPosition)
+            {
+                errors.Add(new ValidationError(ErrorCategory.Malformed, "The Head is positioned outside the HTML"));
+            }
+
+            if(htmlPosition > bodyPosition)
+            {
+                errors.Add(new ValidationError(ErrorCategory.Malformed, "The Body is positioned outside the HTML"));
+            }
+
+            if(bodyPosition > headPosition)
+            {
+                errors.Add(new ValidationError(ErrorCategory.Malformed, "You cannot position the Body inside the Head"));
+            }
+            else if(headPosition > bodyPosition)
+            {
+                errors.Add(new ValidationError(ErrorCategory.Malformed, "You cannot position the Head inside the Body"));
+            }
+             */
 
             return errors;
         }
@@ -25,24 +51,42 @@ namespace Lisa.Raven.Checkers.DefaultCheckers.Controllers
         {
             var errors = new List<ValidationError>();
 
-            
-            
-            return errors;
+            var amount = CountDoctypeRecursive(html.Tree, "");
 
+            if (amount > 1)
+            {
+                errors.Add(new ValidationError(ErrorCategory.Malformed, "Only 1 Doctype in document allowed.", _line, _column));
+            }
+            else if (amount < 1)
+            {
+                errors.Add(new ValidationError(ErrorCategory.CodeStyle, "No doctype found.", _line, _column));
+            }
+
+            if(amount > 1 || amount == 1)
+            {
+                if (_previousElement == "DocumentRoot")
+                {
+                    errors.Add(new ValidationError(ErrorCategory.Malformed, "Doctype must be on the first line of your HTML document.", _line, _column));
+                }
+            }
+
+            return errors;
         }
+
+       
 
         private IEnumerable<ValidationError> CountCheck(SyntaxNode tree, string tag)
         {
             var errors = new List<ValidationError>();
-            var amount = CountRecursive(tree, tag);
+            var amount = CountRecursive(tree, tag, 0, "");
 
             if (amount > 1)
             {
-                errors.Add(new ValidationError(ErrorCategory.CodeStyle, string.Format("Only 1 {0} tag in document allowed.", tag, _line, _column)));
+                errors.Add(new ValidationError(ErrorCategory.Malformed, string.Format("Only 1 {0} tag in document allowed.", tag), _line, _column));
             }
             else if (amount < 1)
             {
-                errors.Add(new ValidationError(ErrorCategory.CodeStyle, string.Format("Only 1 {0} tag in document allowed.", tag), _line, _column));
+                errors.Add(new ValidationError(ErrorCategory.Malformed, string.Format("No {0} tag in document found.", tag), _line, _column));
             }
             return errors;
         }
@@ -59,7 +103,29 @@ namespace Lisa.Raven.Checkers.DefaultCheckers.Controllers
 			return errors;
 		}
 
-        private int CountRecursive(SyntaxNode node, string ElementName)
+        private int CountDoctypeRecursive(SyntaxNode node, string previous)
+        {
+            int amount = 0;
+
+            if (node.Type == SyntaxNodeType.Doctype)
+            {
+                amount++;
+                _column = node.Column;
+                _line = node.Line;
+                _previousElement = previous;
+            }
+
+            previous = node.Value;
+
+            foreach (var child in node.Children)
+            {
+                amount += CountDoctypeRecursive(child, previous);
+            }
+
+            return amount;
+        }
+
+        private int CountRecursive(SyntaxNode node, string ElementName, int treeLocation, string previous)
         {
             int amount = 0;
 
@@ -68,12 +134,15 @@ namespace Lisa.Raven.Checkers.DefaultCheckers.Controllers
                 amount++;
                 _column = node.Column;
                 _line = node.Line;
-                
+                _elementPosition = treeLocation;
+                _previousElement = previous;
             }
+
+            previous = node.Value;
 
             foreach (var child in node.Children)
             {
-                amount += CountRecursive(child, ElementName);
+                amount += CountRecursive(child, ElementName, treeLocation, previous);
             }
 
             return amount;
@@ -81,6 +150,8 @@ namespace Lisa.Raven.Checkers.DefaultCheckers.Controllers
 
         private int _column;
         private int _line;
+        private int _elementPosition;
+        private string _previousElement;
 	}
 
     /*
